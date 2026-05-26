6914134b896817eb98d6d79474bfad386ede7ba7
Pratik <pratikf5@gmail.com>
2026-05-26 13:11:40 +0530

    Update Chapter_04 iterate.js

--- Chapter_04_Arrays/iterate.js
+++ Chapter_04_Arrays/iterate.js
@@ -1,1 +1,25 @@
+let arr = ["test1", "test2", "Test3"]
+const iterator = arr.entries();
 
+console.log("Entries------------");
+for ( const test of iterator)
+{
+    console.log(test)
+}
+
+console.log("Next is for loop---------")
+for (test of arr)
+{
+    console.log(test);
+}
+
+arr.forEach((test, index) => 
+{
+    console.log(test, index)
+})
+
+let students = ["pratik", "appie", "Tusar"]
+for (student in students)
+{
+    console.log(student , students[student])
+}
